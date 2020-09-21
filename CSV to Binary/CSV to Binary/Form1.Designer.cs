542b0d3e99997e4cda680ff24c874cf61be79f0c
kuro-games <kuro@kuro.games>
2020-09-21 20:17:56 +0900

    ADD CSV to Binary Tools

---
+++ CSV to Binary/CSV to Binary/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace CSV_to_Binary
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.OpenFile = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.ChangeFile = new System.Windows.Forms.Button();
+            this.FilePass = new System.Windows.Forms.TextBox();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.saveFileDialog2 = new System.Windows.Forms.SaveFileDialog();
+            this.SuspendLayout();
+            // 
+            // OpenFile
+            // 
+            this.OpenFile.Location = new System.Drawing.Point(12, 12);
+            this.OpenFile.Name = "OpenFile";
+            this.OpenFile.Size = new System.Drawing.Size(75, 23);
+            this.OpenFile.TabIndex = 0;
+            this.OpenFile.Text = "ファイルを開く";
+            this.OpenFile.UseVisualStyleBackColor = true;
+            this.OpenFile.Click += new System.EventHandler(this.OpenFile_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // ChangeFile
+            // 
+            this.ChangeFile.Location = new System.Drawing.Point(12, 66);
+            this.ChangeFile.Name = "ChangeFile";
+            this.ChangeFile.Size = new System.Drawing.Size(75, 23);
+            this.ChangeFile.TabIndex = 1;
+            this.ChangeFile.Text = "変換";
+            this.ChangeFile.UseVisualStyleBackColor = true;
+            this.ChangeFile.Click += new System.EventHandler(this.ChangeFile_Click);
+            // 
+            // FilePass
+            // 
+            this.FilePass.Location = new System.Drawing.Point(12, 41);
+            this.FilePass.Name = "FilePass";
+            this.FilePass.Size = new System.Drawing.Size(758, 19);
+            this.FilePass.TabIndex = 2;
+            this.FilePass.TextChanged += new System.EventHandler(this.FilePass_TextChanged);
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.saveFileDialog1_FileOk);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 100);
+            this.Controls.Add(this.FilePass);
+            this.Controls.Add(this.ChangeFile);
+            this.Controls.Add(this.OpenFile);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "CSV to Binary for SFAI";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button OpenFile;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button ChangeFile;
+        private System.Windows.Forms.TextBox FilePass;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog2;
+    }
+}
+
---
+++ CSV to Binary/CSV to Binary/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace CSV_to_Binary
+{
+    public partial class Form1 : Form
+    {
+        StreamReader sr;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void OpenFile_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Filter = "Scenario Files (*.csv)|*.csv|All files (*.*)|*.*";
+            openFileDialog1.Title = "Open Scenario Files";
+            DialogResult dr = openFileDialog1.ShowDialog();
+            if (dr == System.Windows.Forms.DialogResult.OK)
+            {
+                FilePass.Text = openFileDialog1.FileName;
+            }
+        }
+
+        private void FilePass_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ChangeFile_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.FileName = "新しいファイル.sc";
+            saveFileDialog1.Filter = "Scenario Files (*.sc)|*.sc|All files (*.*)|*.*";
+            saveFileDialog1.Title = "Save Scenario Files";
+            if (FilePass.Text.ToString() == "") return;
+            string readText = File.ReadAllText(@FilePass.Text);
+            if (readText.ToString() == "") return;
+            byte[] data = System.Text.Encoding.UTF8.GetBytes(readText);
+            string whiteText = "";
+            for (int i = 0; data.Length > i; i++)
+            {
+                whiteText += data[i];
+                if (i != data.Length - 1)
+                {
+                    whiteText += ",";
+                }
+            }
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+            }
+            if (saveFileDialog1.FileName.ToString() == "") return;
+            FileStream fs = new FileStream(saveFileDialog1.FileName, FileMode.CreateNew);
+            StreamWriter sw = new StreamWriter(fs, System.Text.Encoding.UTF8);
+            sw.Write(whiteText);
+
+            sw.Close();
+            fs.Close();
+        }
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void saveFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+    }
+}
